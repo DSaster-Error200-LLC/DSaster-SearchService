b04ac19bbd80bf684541b10099c3a0b876f10e69
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2026-09-15 18:46:24 -0600

    feat: add swagger

--- src/dsaster-search/src/main.ts
+++ src/dsaster-search/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module.js";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("DSaster-Search")
+    .setVersion("0.0.1")
+    .build();
+  SwaggerModule.setup("swagger", app, () =>
+    SwaggerModule.createDocument(app, config),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
