7c28c9f9b0cb9d5a1689fe275ad97a35d90c4ef2
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2026-09-15 18:49:43 -0600

    feat: log urls after start

--- src/dsaster-search/src/main.ts
+++ src/dsaster-search/src/main.ts
@@ -1,8 +1,11 @@
+import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module.js";
 
 async function bootstrap() {
+  const logger = new Logger("bootstrap");
+
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
@@ -14,5 +17,9 @@ async function bootstrap() {
   );
 
   await app.listen(process.env.PORT ?? 3000);
+
+  const url = await app.getUrl();
+  logger.log(`Listening on ${url}`);
+  logger.log(`Swagger available on ${url}/swagger`);
 }
 await bootstrap();
