805ed43de566226ea4f668e085dba183dd904392
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2026-09-16 15:01:28 -0600

    chore: fix eslint warnings

--- src/dsaster-search/src/events/events.controller.ts
+++ src/dsaster-search/src/events/events.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get, Query } from "@nestjs/common";
 
-import { ApiOkResponse, ApiQuery } from "@nestjs/swagger";
+import { ApiOkResponse, ApiQuery, ApiTags } from "@nestjs/swagger";
 
 import { Event, EventsService, FindEventsQuery } from "./events.service.js";
 
 @Controller("events")
+@ApiTags("events")
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
--- src/dsaster-search/test/app.e2e-spec.ts
+++ src/dsaster-search/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { INestApplication } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
 
-import { AppModule } from "./../src/app.module.js";
+import { AppModule } from "@app/app.module.js";
 
 describe("AppController (e2e)", () => {
   let app: INestApplication;
