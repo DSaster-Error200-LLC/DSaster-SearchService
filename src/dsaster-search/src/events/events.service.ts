e1b6bfd291cc26b4a1f54114cc662bdd6160fb84
Orlando Rodriguez <rdgzc.27@gmail.com>
2026-09-23 10:39:15 -0600

    chore: open API formater

--- src/dsaster-search/src/events/events.service.ts
+++ src/dsaster-search/src/events/events.service.ts
@@ -1,4 +1,8 @@
-import { ConflictException, Injectable, NotFoundException } from "@nestjs/common";
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
 import { ApiProperty } from "@nestjs/swagger";
 
 import { Type } from "class-transformer";
